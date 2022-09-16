ba6396769d5f4ed77631eb52cd72fbbc47f27731
julio0268 <tuliotorres456@gmail.com>
2022-09-15 22:33:35 -0500

    Se agrego methodo para agg fruta

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const app= new Vue({
             {nombre:'Manzana', cantidad:10},
             {nombre:'Pera', cantidad:0},
             {nombre:'Platano' , cantidad:11},
-            {nombre:'uva' , cantidad:12}],
+            {nombre:'uva' , cantidad:12}
+        ],
+            nuevaFruta:''
+    },
+    methods:{
+        agregarFruta(){
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            })
     }
-});
+}
+})
